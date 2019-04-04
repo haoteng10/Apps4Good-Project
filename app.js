25019d861d93172ec21970b7570da8d0dfac5eb6
Hao <haoteng10@gmail.com>
2019-04-04 00:59:57 +0000

    Some Enhancements

--- app.js
+++ app.js
@@ -12,25 +12,62 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Main GET route
+
 app.get('/', (req, res) => {
   res.render("index");
-  //  res.send("This is sent");
 });
 
+// Fetch the start page
 
 app.get('/start', (req, res) => {
   res.render("start");
-  //  res.send("This is sent");
 });
 
+// USER DATA POST ROUTE
+
+var userData = {
+  name: null,
+  grade: -1
+};
+
+app.post('/start', (req,res) => {
+  userData.name = req.body.player_name;
+  userData.grade = req.body.player_grade;
+  console.log(userData.name + " is playing the game.");
+  res.redirect("/question");
+});
+
+
+// Fetch the question page
 app.get("/question", (req, res) => {
   res.render("question_form");
 });
 
+// Question POST route 
+
+var userQuestionResult = {
+  question_id : null,
+  question_answer: -1
+};
+
+app.post("/question", (req,res) => {
+  // Get the data
+  userQuestionResult.question_id = req.body.id;
+  userQuestionResult.question_answer = req.body.answer;
+  // Compare the data
+  // Redirect to the result page
+  res.redirect("/result");
+});
+
+
+// Fetch the result page
 app.get("/result", (req, res) => {
   res.render("result");
 });
 
+
+// Node.js Application 
 app.listen(process.env.PORT, process.env.IP, () => {
   console.log('Server has started!');
 });
