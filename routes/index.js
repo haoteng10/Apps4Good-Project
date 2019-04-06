609395bb426bafb9fe21a5be95d809091da63e78
Hao <haoteng10@gmail.com>
2019-04-06 02:09:01 +0000

    More stuff

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,8 @@ var userData = {
 };
 
 router.post('/start', (req,res) => {
-  console.log(req.body.player_name);
-  console.log(req.body.player_grade);
+  console.log("The player's name is " + req.body.player_name);
+  console.log("The player's grade level is " + req.body.player_grade);
   if (req.body.player_name !== "" && req.body.player_grade != undefined){
     userData.name = req.body.player_name;
     userData.grade = req.body.player_grade;
@@ -35,20 +35,36 @@ router.post('/start', (req,res) => {
 });
 
 
+// Default DATA
+
 var question_datas = [
         {
             id: 0,
             question: "Use four 6’s and any math signs to make 7. How many ‘+’ signs did you use?",
             selections: ["0", "1", "2", "3"],
             correctChoice: "1"
+        },
+        {
+            id: 1,
+            question: "Find the sum of the digits of 10^11 - 1?",
+            selections: ["11", "9", "10", "12"],
+            correctChoice: "11"
         }
     ];
 
 var chosenQuestion = question_datas[0];
 
+
+// Question randomizer
+function randomizer(passedArray){
+    return Math.floor(Math.random() * passedArray.length);
+}
+
+
 // Question GET route (SHOW the USER a question)
 router.get("/question", (req, res) => {
-  res.render("question_form", {chosenQuestion: chosenQuestion});
+    chosenQuestion = question_datas[randomizer(question_datas)];
+    res.render("question_form", {chosenQuestion: chosenQuestion});
 });
 
 // Question POST route (DATA created by USER)
@@ -64,7 +80,7 @@ router.post("/question", (req,res) => {
   userQuestionResult.question_answer = req.body.answer;
   // Compare the data
   // Redirect to the result page
-  console.log("Here is the result: " + userQuestionResult.question_answer);
+  console.log("The player chose the answer: " + userQuestionResult.question_answer);
   res.redirect("/result");
 });
 
@@ -73,7 +89,10 @@ router.post("/question", (req,res) => {
 function compareAnswers(id, userAnswer){
     var correctQuestionData = question_datas[id];
     if (correctQuestionData.correctChoice == userAnswer) {
+        console.log("The player chose the correct answer.");
         return true;
+    } else {
+        console.log("The player chose the wrong answer. The correct answer is " + correctQuestionData.correctChoice);
     }
     return false;
 }
