0a2339fdb0a95c25687c0d75c59622437e8cc0cf
Hao <haoteng10@gmail.com>
2019-04-28 17:28:52 +0000

    Seeding the DB

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require('path');
+var SeedDB = require("./seeds");
 
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || "0.0.0.0";
@@ -15,6 +16,9 @@ var ip = process.env.IP || "0.0.0.0";
 var url = "mongodb://admin:8Hdm6fxQfLr7K42@ds053305.mlab.com:53305/math-success";
 
 mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false });
+
+// Seeding the DB (Please comment the line below if you want to save the data)
+SeedDB();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/index.js
+++ routes/index.js
@@ -54,45 +54,6 @@ router.post('/start', (req,res) => {
 
 });
 
-
-// Default DATA
-
-var question_datas = [
-        {
-            question: "Use four 6’s and any math signs to make 7. How many ‘+’ signs did you use?",
-            selections: ["0", "1", "2", "3"],
-            correctChoice: "1"
-        },
-        {
-            question: "Find the sum of the digits of 10^11 - 1?",
-            selections: ["11", "9", "10", "12"],
-            correctChoice: "11"
-        }, 
-        {
-          	question: "Find n. 27^8 = 81^N",
-          	selections: ["N = 7", "N = 5", "N = 4", "N = 6"],
-          	correctChoice: "N = 6"
-        },
-        {
-            question: "21y + 67 < 80",
-            selections: ["Y = 2", "Y = 10", "Both of the above", "None of the above"],
-            correctChoice: "None of the above"
-        }, 
-        
-        {
-        	question: "Which gives the largest answer?", 
-        	selections: ["5^3 + 2^8", "7^3 + 1^6", "8^2 + 4^4", "2^7 + 6^3"],
-          correctChoice: "5^3 + 2^8"
-        },
-        
-        {
-        	question: "Find the equation that is TRUE.", 
-        	selections: ["-163 + 145 = 3^0", "(x^2) - 4x + 4 = 0 when x is 2", "99 + 56 - 87 = 66", "(x)(2)(x)(2)(x)(2) = x+8 when x is 1"],
-          correctChoice: "(x^2) - 4x + 4 = 0 when x is 2"
-        }
-
-    ];
-
 function isIncludes(req, inputQuestion){
   var value = false;
   req.session.userData.questions.forEach(function(question){
